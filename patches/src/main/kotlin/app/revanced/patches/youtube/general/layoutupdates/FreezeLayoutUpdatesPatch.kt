fd7095fe7c4f8101b38784f729613e6ab2431877
Nguyễn Tấn Vĩnh <potter.harry0909@yahoo.com>
2026-08-02 17:37:02 +0700

    test

--- patches/src/main/kotlin/app/revanced/patches/youtube/general/layoutupdates/FreezeLayoutUpdatesPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/general/layoutupdates/FreezeLayoutUpdatesPatch.kt
@@ -9,7 +9,8 @@ import app.revanced.patches.youtube.utils.settings.ResourceUtils.addPreference
 import app.revanced.patches.youtube.utils.settings.settingsPatch
 import app.revanced.util.fingerprint.matchOrThrow
 import app.revanced.util.indexOfFirstInstructionOrThrow
-import com.android.tools.smali.dexlib2.Opcode
+import com.android.tools.smali.dexlib2.iface.instruction.ReferenceInstruction
+import com.android.tools.smali.dexlib2.iface.reference.StringReference
 
 private const val EXTENSION_CLASS_DESCRIPTOR = "$UTILS_PATH/FreezeLayoutUpdatesPatch;"
 
@@ -19,63 +20,52 @@ val freezeLayoutUpdatesPatch = bytecodePatch(
     FREEZE_LAYOUT_UPDATES.summary
 ) {
     compatibleWith(COMPATIBLE_PACKAGE)
-
     dependsOn(settingsPatch)
 
     execute {
+        // [BLOCK 1] Xử lý Hot Config
         hotConfigPreferenceFingerprint.matchOrThrow().let { match ->
             match.method.apply {
-                val hotConfigGroupResultIndex = match.stringMatches!!.first().index - 1
-                addInstructions(
-                    hotConfigGroupResultIndex + 1,
-                    """
-                        invoke-static {v5}, $EXTENSION_CLASS_DESCRIPTOR->getHotConfigGroup(Ljava/lang/String;)Ljava/lang/String;
-                        move-result-object v5
-                    """
-                )
+                // Hook Hot Config Group
+                val hotConfigGroupIndex = indexOfFirstInstructionOrThrow { instruction ->
+                    instruction is ReferenceInstruction && instruction.reference is StringReference && (instruction.reference as StringReference).string == "com.google.android.libraries.youtube.innertube.hot_config_group"
+                }
+                addInstructions(hotConfigGroupIndex + 3, """
+                    invoke-static {v1}, $EXTENSION_CLASS_DESCRIPTOR->getHotConfigGroup(Ljava/lang/String;)Ljava/lang/String;
+                    move-result-object v1
+                """)
 
-                val hotStoredTimestampResultIndex = indexOfFirstInstructionOrThrow(hotConfigGroupResultIndex, Opcode.MOVE_RESULT_WIDE)
-                /* This looks like not necessary.
-                addInstructions(hotStoredTimestampResultIndex + 1,
-                    """
-                        invoke-static {v10, v11}, $EXTENSION_CLASS_DESCRIPTOR->getHotStoredTimestamp(J)J
-                        move-result-wide v10
-                    """)
-                 */
-
-                val hotHashDataResultIndex = indexOfFirstInstructionOrThrow(hotStoredTimestampResultIndex, Opcode.INVOKE_INTERFACE) + 1
-                addInstructions(hotHashDataResultIndex + 1,
-                    """
-                        invoke-static {v2}, $EXTENSION_CLASS_DESCRIPTOR->getHotHashData(Ljava/lang/String;)Ljava/lang/String;
-                        move-result-object v2
-                    """)
+                // Hook Hot Hash Data
+                val hotHashDataIndex = indexOfFirstInstructionOrThrow { instruction ->
+                    instruction is ReferenceInstruction && instruction.reference is StringReference && (instruction.reference as StringReference).string == "com.google.android.libraries.youtube.innertube.hot_hash_data"
+                }
+                addInstructions(hotHashDataIndex + 2, """
+                    invoke-static {v1}, $EXTENSION_CLASS_DESCRIPTOR->getHotHashData(Ljava/lang/String;)Ljava/lang/String;
+                    move-result-object v1
+                """)
             }
         }
 
+        // [BLOCK 2] Xử lý Cold Config (Vẫn giữ lại theo yêu cầu)
         coldConfigPreferenceFingerprint.matchOrThrow().let { match ->
             match.method.apply {
-                val coldHashDataResultIndex = match.stringMatches!![2].index + 3
-                addInstructions(coldHashDataResultIndex + 1,
-                    """
-                        invoke-static {v0}, $EXTENSION_CLASS_DESCRIPTOR->getColdHashData(Ljava/lang/String;)Ljava/lang/String;
-                        move-result-object v0
-                    """)
+                // Hook Cold Config Group
+                val coldConfigGroupIndex = indexOfFirstInstructionOrThrow { instruction ->
+                    instruction is ReferenceInstruction && instruction.reference is StringReference && (instruction.reference as StringReference).string == "com.google.android.libraries.youtube.innertube.cold_config_group"
+                }
+                addInstructions(coldConfigGroupIndex + 3, """
+                    invoke-static {v1}, $EXTENSION_CLASS_DESCRIPTOR->getColdConfigGroup(Ljava/lang/String;)Ljava/lang/String;
+                    move-result-object v1
+                """)
 
-                val coldConfigGroupResultIndex = match.stringMatches!![0].index + 3
-                addInstructions(coldConfigGroupResultIndex + 1,
-                    """
-                        invoke-static {v1}, $EXTENSION_CLASS_DESCRIPTOR->getColdConfigGroup(Ljava/lang/String;)Ljava/lang/String;
-                        move-result-object v1
-                    """)
-
-                /* This looks like not necessary.
-                val coldStoredTimestampResultIndex = indexOfFirstInstructionOrThrow(Opcode.MOVE_RESULT_WIDE)
-                addInstructions(coldStoredTimestampResultIndex + 1,
-                    """
-                        invoke-static {v6, v7}, $EXTENSION_CLASS_DESCRIPTOR->getColdStoredTimestamp(J)J
-                        move-result-wide v6
-                    """)
-                 */
+                // Hook Cold Hash Data
+                val coldHashDataIndex = indexOfFirstInstructionOrThrow { instruction ->
+                    instruction is ReferenceInstruction && instruction.reference is StringReference && (instruction.reference as StringReference).string == "com.google.android.libraries.youtube.innertube.cold_hash_data"
+                }
+                addInstructions(coldHashDataIndex + 2, """
+                    invoke-static {v1}, $EXTENSION_CLASS_DESCRIPTOR->getColdHashData(Ljava/lang/String;)Ljava/lang/String;
+                    move-result-object v1
+                """)
             }
         }
 
